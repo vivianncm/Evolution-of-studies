d6a1287f45064c0aef75397e9c41c83395a2394f
Viviann <viviann@actuar.com>
2024-12-03 17:10:03 -0300

    feat: angular design for climate api consumption

--- cursoYouTube/weather-proj/src/app/app.component.spec.ts
+++ cursoYouTube/weather-proj/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'study-Project'`, () => {
+  it(`should have the 'weather-proj' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('study-Project');
+    expect(app.title).toEqual('weather-proj');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, study-Project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, weather-proj');
   });
 });
